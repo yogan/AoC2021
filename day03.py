ff230e2d29518311d8a8eb96821992f535311e68
Frank Blendinger <frank.blendinger@methodpark.de>
2021-12-03 18:44:43 +0100

    Add day 03 part 2 (very ugly!)

--- day03.py
+++ day03.py
@@ -14,6 +14,7 @@ def calc_most_common_bit(numbers, index):
 
     return most_common_bit
 
+
 def part1(lines):
     digits = len(lines[0]) - 1
 
@@ -34,8 +35,44 @@ def part1(lines):
     return gamma * epsilon
 
 
+def has_at_index(digits, value, index):
+    return digits[index] == value
+
+
+def reduce_lines_oxygen(lines, i):
+    most_common_bit = calc_most_common_bit(lines, i)
+    if most_common_bit >= 0:
+        return list(filter(lambda line: has_at_index(line, "1", i), lines))
+    else:
+        return list(filter(lambda line: has_at_index(line, "0", i), lines))
+
+
+def reduce_lines_co2(lines, i):
+    most_common_bit = calc_most_common_bit(lines, i)
+    if most_common_bit < 0:
+        return list(filter(lambda line: has_at_index(line, "1", i), lines))
+    else:
+        return list(filter(lambda line: has_at_index(line, "0", i), lines))
+
+
 def part2(lines):
-    return 0
+    index = 0
+    oxygen_lines = lines
+    while len(oxygen_lines) > 1:
+        oxygen_lines = reduce_lines_oxygen(oxygen_lines, index)
+        index += 1
+
+    oxygen_generator_rating = int(oxygen_lines[0], 2)
+
+    index = 0
+    co2_lines = lines
+    while len(co2_lines) > 1:
+        co2_lines = reduce_lines_co2(co2_lines, index)
+        index += 1
+
+    co2_scrubber_rating = int(co2_lines[0], 2)
+
+    return oxygen_generator_rating * co2_scrubber_rating
 
 
 read_and_solve(__file__, part1, part2)
