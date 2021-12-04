a5d8c60095daafee33a683f7a5efc54e038a213a
Lisa Brueckbauer <lisa.brueckbauer@methodpark.de>
2021-12-04 20:04:11 +0100

    Solve day 4 part 1

--- day04.py
+++ day04.py
@@ -1,5 +1,6 @@
 import re
 from input import read_and_solve
+from functools import reduce
 
 
 def parse_drawn_numbers(line):
@@ -13,22 +14,114 @@ def parse_boards(lines):
             board = []
             continue
         splits = re.split(r'[ ]+', lines[i].strip())
-        board.append(list(map(int, splits)))
-        
+        board.append(list(map(lambda s: (int(s), False), splits)))
+
         if (i+1) % 6 == 0:
             boards.append(board)
 
     return boards
 
 
+def mark_matches(board, number):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j][0] == number:
+                board[i][j] = (number, True)
+    return
+
+
+def board_is_solved(board):
+    for row in board:
+        solved = reduce(lambda a, b: a & b, map(lambda t: t[1], row))
+        if solved:
+            return True
+
+    for i in range(len(board[0])):
+        column = list(map(lambda row: row[i][1], board))
+        solved = reduce(lambda a, b: a & b, column)
+        if solved:
+            return True
+
+    return False
+
+
+def sum_unmarked(board):
+    sum = 0
+    for row in board:
+        for entry in row:
+            if entry[1] == False:
+                sum += entry[0]
+
+    return sum
+
+
 def part1(lines):
     drawn_numbers = parse_drawn_numbers(lines[0])
     boards = parse_boards(lines[1::])
-    return 0
+    for number in drawn_numbers:
+        for board in boards:
+            mark_matches(board, number)
+            if board_is_solved(board):
+                sum = sum_unmarked(board)
+                return sum * number
+
+    raise Exception("No board solved.")
 
 
 def part2(lines):
     return 0
+
+
+def test_sum_unmarked():
+    input_board = [
+        [(22, True), (13, True), (17, False), (11, True), (0, True)],
+        [(8, True), (2, False), (23, False), (4, True), (24, True)],
+        [(21, True), (9, False), (14, True), (16, True), (7, False)],
+        [(6, True), (10, True), (3, False), (18, True), (5, False)],
+        [(1, True), (12, False), (20, True), (15, False), (19, True)]]
+
+    assert sum_unmarked(input_board) == 93
+
+
+def test_board_is_solved():
+    input_board_column_solved = [
+        [(22, True), (13, False), (17, False), (11, False), (0, False)],
+        [(8, True), (2, False), (23, False), (4, False), (24, False)],
+        [(21, True), (9, False), (14, False), (16, False), (7, False)],
+        [(6, True), (10, False), (3, False), (18, False), (5, False)],
+        [(1, True), (12, False), (20, False), (15, False), (19, False)]]
+
+    input_board_row_solved = [
+        [(22, True), (13, True), (17, True), (11, True), (0, True)],
+        [(8, False), (2, False), (23, False), (4, False), (24, False)],
+        [(21, False), (9, False), (14, False), (16, False), (7, False)],
+        [(6, False), (10, False), (3, False), (18, False), (5, False)],
+        [(1, False), (12, False), (20, False), (15, False), (19, False)]]
+
+    input_board_not_solved = [
+        [(22, True), (13, False), (17, True), (11, True), (0, True)],
+        [(8, False), (2, False), (23, False), (4, False), (24, False)],
+        [(21, False), (9, False), (14, False), (16, False), (7, False)],
+        [(6, False), (10, False), (3, False), (18, False), (5, False)],
+        [(1, False), (12, False), (20, False), (15, False), (19, False)]]
+
+    assert board_is_solved(input_board_row_solved) == True
+    assert board_is_solved(input_board_column_solved) == True
+    assert board_is_solved(input_board_not_solved) == False
+
+
+def test_mark_matches():
+    input_board = [
+        [(22, False), (13, False), (17, False), (11, False), (0, False)],
+        [(8, False), (2, False), (23, False), (4, False), (24, False)],
+        [(21, False), (9, False), (14, False), (16, False), (7, False)],
+        [(6, False), (10, False), (3, False), (18, False), (5, False)],
+        [(1, False), (12, False), (20, False), (15, False), (19, False)]]
+    input_number = 2
+    # print(input_board[1][1])
+    mark_matches(input_board, input_number)
+    # print(input_board[1][1])
+    assert input_board[1][1] == (2, True)
 
 
 def test_parse_draw_numbers():
@@ -61,16 +154,20 @@ def test_parse_boards():
     ]
 
     result = parse_boards(input)
-    print(result)
+    # print(result)
     assert len(result) == 3
     assert len(result[0]) == 5
     assert len(result[0][0]) == 5
-    assert result[0][0][0] == 22
-    assert result[0][0][4] == 0
-    assert result[1][1][1] == 18
-    assert result[2][4][4] == 7
+    assert result[0][0][0] == (22, False)
+    assert result[0][0][4] == (0, False)
+    assert result[1][1][1] == (18, False)
+    assert result[2][4][4] == (7, False)
 
 
 test_parse_draw_numbers()
 test_parse_boards()
-#read_and_solve(__file__, part1, part2)
+test_mark_matches()
+test_board_is_solved()
+test_sum_unmarked()
+
+read_and_solve(__file__, part1, part2)
