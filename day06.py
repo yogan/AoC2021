84bd34a17a3ab91364aa8785b6c7da5f78caf159
Lisa Brueckbauer <lisa.brueckbauer@methodpark.de>
2021-12-06 17:00:55 +0100

    Add day 6 part 1

--- day06.py
+++ day06.py
@@ -1,9 +1,32 @@
 from input import read_and_solve
 
+
+def parse_fish(line):
+    return list(map(int, line.split(",")))
+
+
 def part1(lines):
-    return 0
+    days = 80
+    fishes = parse_fish(lines[0])
+
+    for _ in range(days):
+        new_fishes = []
+        spawned_fishes = []
+        for fish in fishes:
+            if fish == 0:
+                new_fishes.append(6)
+                spawned_fishes.append(8)
+            else:
+                new_fishes.append(fish-1)
+
+        new_fishes.extend(spawned_fishes)
+        fishes = new_fishes
+
+    return len(fishes)
+
 
 def part2(lines):
     return 0
+
 
 read_and_solve(__file__, part1, part2)
