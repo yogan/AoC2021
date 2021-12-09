5750e89982f466351f09dea71b4304fffb7fd296
Lisa Brueckbauer <lisa.brueckbauer@methodpark.de>
2021-12-09 15:12:29 +0100

    Add day 9 starter kit

---
+++ day09.py
@@ -0,0 +1,17 @@
+from input import read_and_solve
+
+
+def parse_input(lines):
+    result = []
+    return result
+
+
+def part1(lines):
+    return 0
+
+
+def part2(lines):
+    return 0
+
+
+read_and_solve(__file__, part1, part2)
