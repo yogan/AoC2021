9696028ccff65d82e86ec1d6c74496c650520399
Frank Blendinger <frank.blendinger@methodpark.de>
2021-12-16 00:44:09 +0100

    Add day 15 part 2
    math is hard

--- day15.py
+++ day15.py
@@ -12,6 +12,20 @@ def parse_input(lines):
     return graph
 
 
+def extend(graph):
+    bottom_right = max(graph.keys())
+    x_max = bottom_right[0] + 1
+    y_max = bottom_right[1] + 1
+
+    for x in range(5 * x_max):
+        for y in range(5 * y_max):
+            if x < x_max and y < y_max:
+                continue
+            value = graph[(x % x_max, y % y_max)] \
+                + (x // x_max) + (y // y_max)
+            graph[(x, y)] = value if value < 10 else value % 10 + 1
+
+
 def dijkstra(graph):
     x_max, y_max = max(graph.keys())
 
@@ -42,12 +56,14 @@ def dijkstra(graph):
 
 
 def part1(lines):
-    graph = parse_input(lines)
-    return dijkstra(graph)
+    cave = parse_input(lines)
+    return dijkstra(cave)
 
 
 def part2(lines):
-    return 0
+    cave = parse_input(lines)
+    extend(cave)
+    return dijkstra(cave)
 
 
 class TestDay15(unittest.TestCase):
@@ -66,17 +82,28 @@ class TestDay15(unittest.TestCase):
     ]
 
     def test_parse_input(self):
-        graph = parse_input(["123", "432"])
-        self.assertEqual(graph, {
+        cave = parse_input(["123", "432"])
+        self.assertEqual(cave, {
             (0, 0): 1, (0, 1): 2, (0, 2): 3,
             (1, 0): 4, (1, 1): 3, (1, 2): 2,
+        })
+
+    def test_extend(self):
+        cave = {(0, 0): 8}
+        extend(cave)
+        self.assertEqual(cave, {
+            (0, 0): 8, (0, 1): 9, (0, 2): 1, (0, 3): 2, (0, 4): 3,
+            (1, 0): 9, (1, 1): 1, (1, 2): 2, (1, 3): 3, (1, 4): 4,
+            (2, 0): 1, (2, 1): 2, (2, 2): 3, (2, 3): 4, (2, 4): 5,
+            (3, 0): 2, (3, 1): 3, (3, 2): 4, (3, 3): 5, (3, 4): 6,
+            (4, 0): 3, (4, 1): 4, (4, 2): 5, (4, 3): 6, (4, 4): 7,
         })
 
     def test_part_1_sample(self):
         self.assertEqual(part1(self.sample), 40)
 
-    # def test_part_2_sample(self):
-    #     self.assertEqual(part2(self.sample), TODO)
+    def test_part_2_sample(self):
+        self.assertEqual(part2(self.sample), 315)
 
 
 if __name__ == '__main__':
